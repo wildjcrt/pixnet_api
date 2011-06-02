727cafb7ba5d9a1a666f0f6e8bfc29656b6b5835
Jerry Lee <jcrt@pixnet.tw>
2011-06-02 11:05:16 +0800

    整理code到正確的module

--- lib/pixnet_api/album.rb
+++ lib/pixnet_api/album.rb
@@ -42,5 +42,48 @@ module PixnetApi
     def get_album_element(element_id)
       return JSON.parse(@access_token.get("/album/elements/#{element_id}").body)
     end
+    
+    def upload_pic(set_id, filename)
+      return false unless check_album_set(set_id)
+      
+      url = URI.parse("http://emma.pixnet.cc/album/elements")
+      file = File.new(filename)
+      mime_type = `/usr/bin/file -bi #{file.path}`.strip.split(';')[0]
+      req = Net::HTTP::Post::Multipart.new("/album/elements?set_id=#{set_id}", {
+        "upload_file" => UploadIO.new(file, mime_type, file.path),
+      })
+      
+      @access_token.sign!(req)
+      Net::HTTP.new(url.host, url.port).start do |http|
+        res = http.request(req)
+        return JSON.parse(res.body)
+      end
+    end
+
+    def upload_pic_by_url(set_id, pic_url)
+      return false unless check_album_set(set_id)
+      
+      require 'open-uri'
+      
+      url = URI.parse("http://emma.pixnet.cc/album/elements")
+      
+      begin
+        io = open(pic_url)
+      rescue
+        return false
+      end
+      
+      return false unless (io.meta['content-length'].to_i > 0) && io.meta['content-type'].match(/^image/)
+      
+      req = Net::HTTP::Post::Multipart.new("/album/elements?set_id=#{set_id}", {
+        "upload_file" => UploadIO.new(io, io.meta['content-type']),
+      })
+      
+      @access_token.sign!(req)
+      Net::HTTP.new(url.host, url.port).start do |http|
+        res = http.request(req)
+        return JSON.parse(res.body)
+      end
+    end
   end
 end
